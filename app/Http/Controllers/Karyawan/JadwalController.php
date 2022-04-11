463479dddc66c7c3b7143a8fb3165ba996cad90a
muhalimashar21 <39429176+muhalimashar21@users.noreply.github.com>
2022-04-11 15:59:21 +0700

    middleware

--- app/Http/Controllers/Karyawan/JadwalController.php
+++ app/Http/Controllers/Karyawan/JadwalController.php
@@ -10,110 +10,36 @@ use Illuminate\Support\Facades\DB;
 
 class JadwalController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index(Request $request)
     {
 
-        if (request()->ajax()) {
+        $jadwal = Jadwal::select('user_id as title','tanggal_mulai as start','shift_id as end')->get();
 
-            $data = Jadwal::with('shift','user')
-            ->select('id', 'user_id', 'tanggal_mulai', 'tanggal_selesai', 'shift_id')
-            ->where('tanggal_mulai', '>=', $request->get('from'))
-            ->where('tanggal_selesai', '<=', $request->get('to'))
-            ->orderByDesc('tanggal_mulai');
-
-            return Datatables::of($data->get())
-                ->addIndexColumn()
-                ->addColumn('tgl_mulai', function ($data) {
-                    return date("d F Y", strtotime($data->tanggal_mulai));
-                })
-                ->addColumn('tgl_selesai', function ($data) {
-                    return date("d F Y", strtotime($data->tanggal_selesai));
-                })
-                ->addColumn('action', function ($data) {
-                        $actionBtn = '
-                                        <center>
-                                        <a href="javascript:void(0)" class="btn btn-outline-primary btn-sm" data-toggle="tooltip" data-placement="top" title="Bayar" onclick="bayar(' . $data->id . ')"><i class="ti-money"></i></a>
-                                      
-                                        </center>';
-                    return $actionBtn;
-                })
-                ->rawColumns(['tgl_mulai','tgl_selesai','action'])
-                ->make(true);
-        }
-
-        return view('karyawan.jadwal.index',[
-            'title' => 'Jadwal'
+        return view('karyawan.jadwal.index', compact('jadwal'), [
+            'title'     => 'Kalender'
         ]);
+        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $cek = Jadwal::with('shift','user')
+        ->select('tanggal_mulai','shift_id','user_id')
+        ->get();
+
+        foreach($cek as $data){
+            $array = [
+                'title'  => $data->shift->nama,
+                'start'  => $data->tanggal_mulai .' '. $data->shift->jam_masuk,
+                'end'    => $data->tanggal_mulai .' '. $data->shift->jam_pulang,
+            ];
+
+            $result[] = $array;
+        }
+
+        return $result;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
